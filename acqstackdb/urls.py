b6f1d7f2054ec5e8be5fcd3bde5524573466ca43
stvnrlly <steven.reilly@gsa.gov>
2016-05-31 12:25:20 -0400

    skeleton of site

--- acqstackdb/urls.py
+++ acqstackdb/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from acquisitions import views
+
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- acquisitions/views.py
+++ acquisitions/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Acquisition
 
 # Create your views here.
+def home(request):
+    acquisitions = Acquisition.objects.all()
+    return render(request, "acquisitions/index.html", {"acquisitions":acquisitions})
