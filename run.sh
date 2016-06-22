4908cceb3c927b6f686871ed1e1a318ee47a06dc
V David Zvenyach <vladlen.zvenyach@gsa.gov>
2016-06-22 08:04:00 -0400

    Added documentation to README and handled graceful postgres loading
    The documentation for using docker-compose is now included.
    
    Additionally, added a `wait-for-it` wrapper to ensure that postgres is
    up and running before running the web application. Kudos to @vishnubob for
    his excellent script.

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-python manage.py migrate && python manage.py runserver 0.0.0.0:5000
+# Per https://docs.docker.com/compose/startup-order/, we are going to use
+# wait-for-it (source: https://github.com/vishnubob/wait-for-it) as a wrapper
+# to ensure that postgres is ready to accept connections
+./third-party/wait-for-it/wait-for-it.sh db:5432
+
+# Now, run the script
+python manage.py migrate
+python manage.py runserver 0.0.0.0:5000
